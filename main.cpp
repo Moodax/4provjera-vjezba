6169f2dfd4af5322579edb4423ff3623e9cf2f6b
Moodax <64415905+Moodax@users.noreply.github.com>
2020-04-30 15:12:56 +0200

    Update main.cpp
    Korisniku se prikazuje glavni izbornik (unos novog računa, ispiši sve podatke, pretraga
    prema prezimenu i imenu, brisanje računa, izmjena podataka prema broju računa, ispiši
    sortirano po prezimenu i imenu, izlaz iz programa) u beskonačnoj petlji.
    Korisnik unosi novi račun:
    ● 10-ero znamenkasti broj računa (dinamički alocirano polje tipa ​unsigned long long
    int​ veličine 1000) - ​try-catch:​ korisniku se ispisuje greška o neispravnom broju
    računa te se ponovno traži upis, broj mora imati 10 znamenki
    ● prezime i ime (dinamički alocirano polje tipa ​string ​veličine 1000)
    ● saldo (dinamički alocirano polje tipa ​double ​veličine 1000)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
-
+#include<math.h>
+#include<algorithm>
+#include<string>
+#include<utility>
+#include<numeric>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int brklijenata=0;
+    unsigned long long int *brracuna=new unsigned long long int [1000];
+    string *ime=new string [1000];
+    double *saldo=new double[1000];
+    unsigned long long int unos;
+    string unoss;
+    int izbor;
+    while(1)
+    {
+        cout<<"1. Unos novog racuna"<<endl;
+        cout<<"2. Ispisi sve podatke"<<endl;
+        cout<<"3. Pretraga prema prezimenu i imenu"<<endl;
+        cout<<"4. Brisanje racuna"<<endl;
+        cout<<"5. Ispisi sortirano po prezimenu i imenu"<<endl;
+        cout<<"6. Izmjena podataka prema broju racuna"<<endl;
+        cout<<"7. Izlaz iz programa"<<endl;
+        cin>>izbor;
+        if(izbor==1)
+        {
+            while(1)
+            {
+        try {
+                    cout<<"Unesite broj racuna ";
+                    cin>>unos;
+                    if(unos<1000000000 || unos>9999999999)
+                            throw  "Broj racuna mora imati 10 znamenaka !";
+                    break;
+        } catch (const char* msg) {
+            cout<<msg<<endl;
+        }
+            }
+
+        brracuna[brklijenata]=unos;
+        cout<<"Unesite prezime i ime: ";
+        cin.ignore();
+        getline(cin,ime[brklijenata]);
+        cout<<"Unesite saldo: "<<endl;
+        cin>>saldo[brklijenata];
+        brklijenata++;
+    }
+}
     return 0;
 }
+
